9fc3fc0918cae547b3ae87a49d139f6bf18ec0b3
Kaizne <kaizne@kaizne.com>
2023-03-15 17:33:07 -0700

    Duplicate name fix

--- convert-webp.js
+++ convert-webp.js
@@ -6,6 +6,6 @@ const name = image.split('.')[0]
 
 sharp(image)
     .toFormat('webp', { quality:50 })
-    .toFile(`${name}.webp`)
+    .toFile(`${name}2.webp`)
     .then(info => console.log(info))
     .catch(err => console.log(err))
