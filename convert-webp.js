4845e01890af98ea0b24284b9b24bc8adbb42cee
Kaizne <kaizne@kaizne.com>
2023-05-26 19:16:56 -0400

    Same extension fix

--- convert-webp.js
+++ convert-webp.js
@@ -3,9 +3,20 @@ const sharp = require('sharp')
 const prompt = require('prompt-sync')({ sigint:true })
 const image = prompt('Enter image file name: ')
 const name = image.split('.')[0]
+const extension = image.split('.')[1]
 
-sharp(image)
-    .toFormat('webp', { quality:50 })
-    .toFile(`${name}2.webp`)
-    .then(info => console.log(info))
-    .catch(err => console.log(err))
+if (extension === 'webp') {
+    sharp(image)
+        .toFormat('webp', { quality:50 })
+        .toFile(`${name}2.webp`)
+        .then(info => console.log(info))
+        .catch(err => console.log(err))
+}
+else {
+    sharp(image)
+        .toFormat('webp', { quality:50 })
+        .toFile(`${name}.webp`)
+        .then(info => console.log(info))
+        .catch(err => console.log(err))
+}
+
